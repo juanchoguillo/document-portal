5e06d2fde61eccfbf7ec6c3806b9b4f9f5099b21
juanchoguillo <juancardona0607@gmail.com>
2025-07-19 07:52:20 -0400

    end of first class

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='document_portal',
+    author="Juan Cardona",
+    author_email="juan@bizbridge.ai",
+    version='0.1',
+    description='A document management portal built with FastAPI',
+    packages=find_packages(),
+)       
